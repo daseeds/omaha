738b603f5211d2862fb7910b46635898bb866262
daseeds <cyril.jean@gmail.com>
2013-09-27 12:46:50 +0800

    one more pic

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ window.App = Ember.Application.create({
   ready: function() {
     //initialize router, controller and views
     
-    $.backstretch([   "static/img/la_tour_1_1_.JPG",   "static/img/entree.jpg",   "static/img/L1014010-2.jpg"    ], {duration: 10000});
+    $.backstretch([  "static/img/DSC_0403.JPG", "static/img/la_tour_1_1_.JPG",   "static/img/entree.jpg",   "static/img/L1014010-2.jpg"    ], {duration: 10000});
 
   }
 });
