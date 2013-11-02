c7a09257a1b87c361a88ba911c265963400e4152
Cyril Jean <cyril.jean@gmail.com>
2013-11-02 15:12:50 +0800

    the model is now working

--- static/js/models/slide.js
+++ static/js/models/slide.js
@@ -8,13 +8,18 @@ App.FIXTURES = [
        id: 'bed-and-breakfeast-manor-in-normandy',
        lang: 'en',
        name: 'THE MANOR',
-       title: 'XVI CENTURY MANOR HOUSE',
-       extra_title: 'Holiday in Normandy',
+       catch_phrase: 'XVI CENTURY MANOR HOUSE',
+       sub_title: 'Holiday in Normandy',
+       title: 'The Manor',
        articles: [
          {
           id: 1,
-          title: 'coucou'
+          content: "Looking for holiday accomodation in Normandy ? Our XVI century Manor House is an excellent base from which to explore the attractions of the area. It’s within easy – driving distance of Sainte Mère Eglise, the D.Day Landing Beaches, Bayeux and Le Mont St Michel."
+         },{
+          id: 2,
+          content: "Our Manor is nestled in a very quiet country setting surrounded by gardens where you can relax on a summer evening. Our non-smoking rooms have been decorated to create an intimate atmosphere: refined but relaxed, combining charm and comfort. Breakfast is particularly appreciated with home made products."
          }
+
        ],
        pictures: [
         {
@@ -26,18 +31,167 @@ App.FIXTURES = [
         }
        ]
      }, {
-       id: 'bed-and-breakfeast-manor-in-normandy-garden',
+       id: 'bed-and-breakfeast-manor-in-normandy-room-la-tour',
        lang: 'en',
-       name: 'THE GARDEN',
-       title: 'WOW SO GREEN',
-       extra_title: 'Holiday in Normandy',
+       name: "ROOM 'LA TOUR'",
+        catch_phrase: '90 €',
+       sub_title: 'per room per night, for 2 persons',
+        title: "Room 'La Tour'",
+        articles: [
+         {
+          id: 3,
+          content: "Nestling on the 2nd floor of the manor, this spacious and bright room is finished in moderate tones, enhanced with ochre to match the exposed ceiling beams. A sofa and an armchair enable our guests to read and relax. "
+
+         },{
+          id: 3,
+          content: "Bedding : a queen size bed and a single bed."
+         }
+       ],
        pictures: [
         {
-          id: 1,
-          path: 'static/img/DSC_0403.JPG'
-        },{
-          id: 2,
+          id: 3,
           path: 'static/img/la_tour_1_1_.JPG'
+        }
+       ]
+     }, {
+       id: 'bed-and-breakfeast-manor-in-normandy-room-baldaquin',
+       lang: 'en',
+       name: "ROOM 'BALDAQUIN'",
+        catch_phrase: '90 €',
+       sub_title: 'per room per night, for 2 persons',
+        title: "Room 'Baldaquin'",
+        articles: [
+         {
+          id: 3,
+          content: "Situated on the 1st floor this large room affords a lovely view over the garden and the stables. The main feature of this bright room is the canopy bed. The atmosphere created is warm and relaxing."
+
+         },{
+          id: 3,
+          content: "Bedding : a queen size bed and a single bed."
+         }
+       ],
+       pictures: [
+        {
+          id: 3,
+          path: 'static/img/baldaquin_1.JPG'
+        },        {
+          id: 3,
+          path: 'static/img/salle_de_bains_017.jpg'
+        }
+       ]
+     }, {
+       id: 'bed-and-breakfeast-manor-in-normandy-room-exotique',
+       lang: 'en',
+       name: "ROOM 'EXOTIQUE'",
+        catch_phrase: '75 €',
+       sub_title: 'per room per night, for 2 persons',
+        title: "Room 'Exotique'",
+        articles: [
+         {
+          id: 3,
+          content: "This ochre coloured room, enhanced with traditional embroidery is spacious and comfortable."
+
+         },{
+          id: 3,
+          content: "Bedding : a queen size bed and a single bed."
+         }
+       ],
+       pictures: [
+        {
+          id: 3,
+          path: 'static/img/chambre_exotique_1.JPG'
+        },        {
+          id: 3,
+          path: 'static/img/salle_de_bains_017.jpg'
+        }
+       ]
+     }, {
+       id: 'bed-and-breakfeast-manor-in-normandy-familty-suite',
+       lang: 'en',
+       name: "FAMILY SUITE",
+        catch_phrase: '120 €',
+       sub_title: 'per room per night, for 4 persons',
+        title: "Family Suite",
+        articles: [
+         {
+          id: 3,
+          content: "You will feel right at home in the suite comprising two en suite bedrooms. The cosy parents' room with its queen size bed has been decorated to create a relaxing atmosphere. The children meanwhile have their own space in a nice room with twin beds and a “ciel de lit”."
+
+         }
+       ],
+       pictures: [
+        {
+          id: 3,
+          path: 'static/img/romantique1_1_.JPG'
+        },        {
+          id: 3,
+          path: 'static/img/suite-2-grande.jpg'
+        }
+       ]
+     }, {
+       id: 'bed-and-breakfeast-manor-in-normandy-big-green-garden-relax',
+       lang: 'en',
+       name: 'THE GARDEN',
+        catch_phrase: 'WOW SO GREEN YA~',
+       sub_title: 'Holiday in Normandy',
+        title: 'The Garder',
+        articles: [
+         {
+          id: 3,
+          content: "A garden area is provided for guest enjoyment and relaxation. Guest parking is private, off the road and convenient to the guest entrances"
+         }
+       ],
+       pictures: [
+        {
+          id: 3,
+          path: 'static/img/DSC_0597.JPG'
+        },{
+          id: 4,
+          path: 'static/img/DSC_0617-1.jpg'
+        }
+       ]
+     },{
+       id: 'bed-and-breakfeast-manor-in-normandy-surroundings-beach-1944',
+       lang: 'en',
+       name: 'SURROUNDINGS',
+        catch_phrase: 'BLAH BLAH BLAH',
+       sub_title: 'Holiday in Normandy',
+        title: 'The Surroundings',
+        articles: [
+         {
+          id: 3,
+          content: "Close to the Manor : Golf, Horse -riding, sailing, swimming, fishing, hiking."
+         },{
+          id: 3,
+          content: "D Day beaches : Utah Beach, Omaha beach"
+         },{
+          id: 3,
+          content: "Preserved Areas : La Baie du Mont Saint Michel, Les Falaises de la Hague, Les Landes de Vauville, Parc Naturel des Marais..."
+         }
+       ],
+       pictures: [
+        {
+          id: 3,
+          path: 'static/img/DSC_0403.JPG'
+        }
+       ]
+     },{
+       id: 'bed-and-breakfeast-manor-in-normandy-book-booking',
+       lang: 'en',
+       name: 'BOOK',
+        catch_phrase: 'BLAH BLAH BLAH',
+       sub_title: 'Holiday in Normandy',
+        title: 'Book',
+        articles: [
+         {
+          id: 3,
+          content: "Todo: add address and contact form for direct mail sending. Photo en gros plan d'un agenda ouvert"
+         }
+       ],
+       pictures: [
+        {
+          id: 3,
+          path: 'static/img/DSC_0403.JPG'
         }
        ]
      }
@@ -50,8 +204,17 @@ App.FIXTURES = [
        id: 'chambres-hotes',
        lang: 'fr',
        name: 'LE MANOIR',
-       title: 'UN MANOIR DU 16eme VOUS ATTENDS',
-       extra_title: 'Vous ressourcer en Normandie',
+      catch_phrase: 'UN MANOIR DU 16eme VOUS ATTENDS',
+       sub_title: 'Vous ressourcer en Normandie',
+       pictures: [
+        {
+          id: 1,
+          path: 'static/img/entree.jpg'
+        },{
+          id: 2,
+          path: 'static/img/L1014010-2.jpg'
+        }
+       ]
      }
   ]  
 }
--- static/js/router.js
+++ static/js/router.js
@@ -24,21 +24,43 @@ App.ApplicationRoute = Em.Route.extend({
   model: function() {
     return App.Locale.find();
   },
-  afterModel: function(model, transition) {
+/*  afterModel: function(model, transition) {
       this.transitionTo('locale', model[0]);
-    }
+    },*/
+/*  setupController: function(controller, model) {
+    console.log(model); 
+  }*/
 });
 
 App.LocaleRoute  = Em.Route.extend({
   model: function(params) {
     return App.Locale.find(params.locale_id);
   },
-/*  afterModel: function(model, transition) {
-      this.transitionTo('page', model[0]);
-    }*/
+  afterModel: function(model, transition) {
+    console.log("App.LocaleRoute:afterModel " + model);
+      //this.transitionTo('page', model.pages[0]);
+  }
+/*  setupController: function(controller, model) {
+    console.log(model); 
+  }*/
 });
 
+App.ApplicationController = Em.ArrayController.extend({
+  destinationRoute: 'target'
+
+});
+
+
+App.LocaleController = Ember.ObjectController.extend({
+  needs: "page",
+  page: Ember.computed.alias("controllers.page"),
+  isVisible: true
+});
+
+
 App.PageController = Ember.ObjectController.extend({
+  needs: "locale",
+  locale: Ember.computed.alias("controllers.locale"),
   ready: function() {
     console.log('activate )' + model.toArray());
   }
@@ -47,11 +69,28 @@ App.PageController = Ember.ObjectController.extend({
 App.PageRoute = Em.Route.extend({
   model: function(params) {
     console.log("params.page_id: " + params.page_id);
-    var model = this.modelFor('locale').pages.findBy('id', params.page_id);
+    return this.modelFor('locale').pages.findBy('id', params.page_id);
+  },
+  actions: {
+    willTransition: function(transition) {
+      console.log("destroy backstretch before transition");
+      $('body').backstretch("destroy", false);
+     }
+  },
+  afterModel: function(model, transition) {
+    var backStrechArrayParam = [];
+    
+    for (var i = 0; i < model.pictures.length; i++) {
+      backStrechArrayParam[i] = model.pictures[i].path;
+    }
+    var instance = $('body').data('backstretch');
+    if (instance != undefined)
+      $('body').backstretch("destroy", true);
 
-    $.backstretch([model.pictures[0].path, model.pictures[1].path]);
-
-    return model;
+    $('body').backstretch(backStrechArrayParam, {duration: 3000, fade: 750});
+    var instance = $('body').data('backstretch');
+    if (instance != undefined)
+      console.log("after: " + instance.images.toString());
   }
 });
 
